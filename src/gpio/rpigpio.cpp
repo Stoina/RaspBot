3f4eb9e1a18ffb8a5d3c626a0d4c053308f52157
Julian Steiner <julian_steiner7@outlook.com>
2020-03-18 08:52:29 +0100

    Add source files for gpio control.

---
+++ src/gpio/rpigpio.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#include "rpigpio.h"
+
+#ifdef __unix__
+
+#define IsUnixOs
+
+#include <sys/types.h>
+#include <sys/mman.h>
+
+#endif
+
+void initialize_bcm_board()
+{
+    int* mem_fd;
+    if (open_dev_mem_file(mem_fd) > 0)
+    {
+        map_mem(mem_fd);
+    }
+}
+
+int open_dev_mem_file(int* ptr_mem_fd)
+{
+    int mem_fd = open("/dev/mem", O_RDWR|O_SYNC);
+
+    ptr_mem_fd  = &mem_fd;
+
+    if (mem_fd  > 0)
+    {
+        printf("Error while mapping physical gpio-register in virtual memory");
+    }
+
+    return mem_fd;
+}
+
+void map_mem(int* mem_fd)
+{
+    void* map = mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, *mem_fd, GPIO_BASE);
+    
+    if (map == (__caddr_t)-1)
+    {
+        printf("Error mapping memory");
+    }
+    else
+    {
+        printf("Successfully mapped memory");
+    }
+}
---
+++ src/gpio/rpiogpio.h
@@ -0,0 +1,35 @@
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE (4*1024)
+#endif
+
+#ifndef BCM2708_GPIO
+#define BCM2708_GPIO 0x20000000
+#endif
+
+// virtual address from BCM2835 CPU
+// in the documentation the address is 0x7E.... but because the MMU we must change to 0x20....
+#ifndef BCM2835_GPIO
+#define BCM2835_GPIO 0x200000
+#endif
+
+#define GPIO_BASE (BCM2708_GPIO + BCM2835_GPIO)
+
+// define gpio select functions in 3-Bit
+#ifndef GPIO_SEL_FUNCTIONS
+#define GPIO_SEL_FUNCTIONS
+
+#define GPIO_OUT 0b000
+#define GPIO_IN  0b001
+#define GPIO_ALTERNATE_0 0b100
+#define GPIO_ALTERNATE_1 0b101
+#define GPIO_ALTERNATE_2 0b110
+#define GPIO_ALTERNATE_3 0b111
+#define GPIO_ALTERNATE_4 0b011
+#define GPIO_ALTERNATE_5 0b010
+
+#endif
+
+int main();
+void initialize_bcm_board();
+int open_dev_mem_file(int* ptr_mem_fd);
+void map_mem(int* mem_fd);
