ce7542fd3105fa5735dafa9df622c357cb09c716
Julian Steiner <julian_steiner7@outlook.com>
2020-03-18 12:14:01 +0100

    Pointer fixes.

--- src/gpio/rpigpio.cpp
+++ src/gpio/rpigpio.cpp
@@ -43,17 +43,17 @@ int open_dev_mem_file(const char* dev_mem_file_name, int* ptr_mem_fd)
     std::cout << "Try to open file: " << dev_mem_file_name << "..." << std::endl;
     
     int mem_fd = open(dev_mem_file_name, O_RDWR|O_SYNC);
+    ptr_mem_fd  = &mem_fd;
+
     if (mem_fd < 0)
     {
-        std::cout << "Error while mapping physical gpio-register in virtual memory. File descriptor: " << mem_fd << std::endl;
+        std::cout << "Error while mapping physical gpio-register in virtual memory. File descriptor: " << *ptr_mem_fd << std::endl;
     }
     else
     {
-        std::cout << "File: " << dev_mem_file_name << " Descriptor: " << mem_fd << std::endl;
+        std::cout << "File: " << dev_mem_file_name << " Descriptor: " << *ptr_mem_fd << std::endl;
     }
-
-    ptr_mem_fd  = &mem_fd;
-
+    
     return mem_fd;
 }
 
