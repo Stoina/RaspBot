2ca86bcb1131ad9726cb22f10e2a72f109bc90af
Julian Steiner <julian_steiner7@outlook.com>
2020-03-18 12:11:38 +0100

    Add logging.

--- src/gpio/rpigpio.cpp
+++ src/gpio/rpigpio.cpp
@@ -60,6 +60,7 @@ int open_dev_mem_file(const char* dev_mem_file_name, int* ptr_mem_fd)
 void map_mem(int* mem_fd)
 {
     std::cout << "Map memory.." << std::endl;
+    std::cout << "File descriptor pointer: " << mem_fd << " value: " << *mem_fd;
 
     void* map = mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, *mem_fd, GPIO_BASE);
     
