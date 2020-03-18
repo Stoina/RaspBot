c86e27b72e89da6315a567398f82f4372846eba8
Julian Steiner <julian_steiner7@outlook.com>
2020-03-18 11:41:28 +0100

    Pointer fix.

--- src/gpio/rpigpio.cpp
+++ src/gpio/rpigpio.cpp
@@ -65,7 +65,7 @@ int open_dev_mem_file(const char* dev_mem_file_name, int* ptr_mem_fd)
 
 void map_mem(int* mem_fd)
 {
-    void* map = mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, &mem_fd, GPIO_BASE);
+    void* map = mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, *mem_fd, GPIO_BASE);
     
     if (map == (__caddr_t)-1)
     {
