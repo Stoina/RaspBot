6346939e4095e7919eda54507304a1445f445541
Julian Steiner <julian_steiner7@outlook.com>
2020-03-18 11:34:34 +0100

    Fix bugs in rpi gpio.

--- src/gpio/rpigpio.cpp
+++ src/gpio/rpigpio.cpp
@@ -20,11 +20,11 @@ void hello_gpio()
 void initialize_bcm_board()
 {
     int* mem_fd;
-    if (open_dev_mem_file(mem_fd) > 0)
+    if (open_dev_mem_file(mem_fd) >= 0)
     {
         map_mem(mem_fd);
     }
-    else if (open_dev_gpiomem_file(mem_fd) > 0)
+    else if (open_dev_gpiomem_file(mem_fd) >= 0)
     {
         map_mem(mem_fd);
     }
@@ -52,7 +52,7 @@ int open_dev_mem_file(char* dev_mem_file_name, int* ptr_mem_fd)
 
     ptr_mem_fd  = &mem_fd;
     
-    if (mem_fd  > 0)
+    if (mem_fd < 0)
     {
         std::cout << "Error while mapping physical gpio-register in virtual memory" << std::endl;
     }
@@ -62,7 +62,7 @@ int open_dev_mem_file(char* dev_mem_file_name, int* ptr_mem_fd)
 
 void map_mem(int* mem_fd)
 {
-    void* map = mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, *mem_fd, GPIO_BASE);
+    void* map = mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, &mem_fd, GPIO_BASE);
     
     if (map == (__caddr_t)-1)
     {
