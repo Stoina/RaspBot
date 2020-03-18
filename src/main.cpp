364f00357207219ff456c025756ffb00742d935c
Julian Steiner <julian_steiner7@outlook.com>
2020-03-18 08:52:41 +0100

    Initialize GPIO board.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "gpio/rpigpio"
 
 int main() 
 {
     std::cout << "Hello World - RaspBot" << std::endl;
+    
+    std::cout << "Initialize Gpios..." << std::endl;
+    initialize_bcm_board();
 }
